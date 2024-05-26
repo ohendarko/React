7fa4a7dd335a9e0119ad3954a538fbd3870819cd
ohendarko <ohendarko@gmail.com>
2024-05-26 19:04:57 +0000

    I lost track of everything Ive done

--- Traversy_Media/react-crash-2024/src/App.jsx
+++ Traversy_Media/react-crash-2024/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import HomePage from './pages/HomePage';
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={<HomePage />} />)
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFoundPage />} />
+    </Route>
+  )
 );
 
 const App = () => {
