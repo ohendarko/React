3b032adba6af20053aeb423f4e0d805ccfcb55ad
ohendarko <ohendarko@gmail.com>
2024-06-19 12:42:47 +0000

    its beautiful

--- freecodecamp/fcc-airbnb-experiences/src/App.jsx
+++ freecodecamp/fcc-airbnb-experiences/src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './Components/Navbar';
 import PhotoGrid from './Components/PhotoGrid';
 import Hero from './Components/Hero';
 import Card from './Components/Card';
-import loneWoman from './assets/images/lone-woman.png';
+
 
 const App = () => {
   return (
@@ -12,13 +12,7 @@ const App = () => {
       <Navbar />
       <PhotoGrid />
       <Hero />
-      <Card
-      img={loneWoman}
-      rating="5.0"
-      reviewCount={6}
-      country="USA"
-      
-      />
+      <Card />
     </div>
   )
 }
--- freecodecamp/fcc-airbnb-experiences/src/Components/Card.jsx
+++ freecodecamp/fcc-airbnb-experiences/src/Components/Card.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import loneWoman from '../assets/images/lone-woman.png';
+import loneWoman from "../assets/images/lone-woman.png";
+import cards from '../cards.json';
 
 const Card = () => {
+  console.log(cards);
   return (
     <div className='card-container'>
-      <div className='card'>
-        <img className='card-image' src={loneWoman} />
-        <p>5.0 <span className='gray'>(6) * USA</span></p>
-        <p>Life lessons with Katie Zaferes</p>
-        <p><span className='bold'>From $136</span> / person</p>
-      </div>
+      {cards.map((card) => (
+        <div className='card'>
+          <img className='card-image' src={card.img} />
+          <p>{card.rating} <span className='gray'>({card.reviewCount}) * {card.country}</span></p>
+          <p>{card.title}</p>
+          <p><span className='bold'>From ${card.price}</span> / person</p>
+        </div>
+      ))}
     </div>
   )
 }
