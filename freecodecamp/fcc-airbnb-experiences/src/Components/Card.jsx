5bd15faa908f6d3f0504b6e8fecf846ed350c15c
ohendarko <ohendarko@gmail.com>
2024-06-19 13:37:59 +0000

    miniproject complete

--- freecodecamp/fcc-airbnb-experiences/src/Components/Card.jsx
+++ freecodecamp/fcc-airbnb-experiences/src/Components/Card.jsx
@@ -3,12 +3,12 @@ import loneWoman from "../assets/images/lone-woman.png";
 import cards from '../cards.json';
 
 const Card = () => {
-  console.log(cards);
   return (
     <div className='card-container'>
       {cards.map((card) => (
         <div className='card'>
           <img className='card-image' src={card.img} />
+          {card.openSpots === 0 && <div className='status'><small>SOLD OUT!</small></div>}
           <p>{card.rating} <span className='gray'>({card.reviewCount}) * {card.country}</span></p>
           <p>{card.title}</p>
           <p><span className='bold'>From ${card.price}</span> / person</p>
